81f61ea90b0281f61be47a859bd3334a6ba750a5
rasinchungath <rasincrazz@gmail.com>
2023-01-21 12:43:47 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// main.dart
+import 'package:flutter/material.dart';
+import 'package:sql_flutter/home.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        // Remove the debug banner
+        debugShowCheckedModeBanner: false,
+        title: 'SQLite demo',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+        ),
+        home: const HomePage());
+  }
+}
+
